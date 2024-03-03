9c4a18f11e0d71a6f2bc3178ab85679c284c8923
Vo Le hong <=>
2024-03-03 15:17:37 +0700

    [Volh][Blockchain][Rust][Main functioin update update version]

---
+++ target/release/deps/safemath_dev-06ba1a8de9ca2f4f.d
@@ -0,0 +1,8 @@
+/Users/nguyenductrang/Desktop/Contract/lib/lib_rust_dev/target/release/deps/libsafemath_dev-06ba1a8de9ca2f4f.rmeta: src/lib.rs src/math/mod.rs
+
+/Users/nguyenductrang/Desktop/Contract/lib/lib_rust_dev/target/release/deps/libsafemath_dev-06ba1a8de9ca2f4f.rlib: src/lib.rs src/math/mod.rs
+
+/Users/nguyenductrang/Desktop/Contract/lib/lib_rust_dev/target/release/deps/safemath_dev-06ba1a8de9ca2f4f.d: src/lib.rs src/math/mod.rs
+
+src/lib.rs:
+src/math/mod.rs:
