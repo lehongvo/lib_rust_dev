892d5c5015eea8ac41d0990b452582d67996ecd7
Vo Le hong <=>
2024-03-03 15:15:41 +0700

    [Volh][Blockchain][Rust][Main functioin update update version]

---
+++ target/debug/deps/safemath_dev-3adb08666a7cc635.d
@@ -0,0 +1,6 @@
+/Users/nguyenductrang/Desktop/Contract/lib/lib_rust_dev/target/debug/deps/libsafemath_dev-3adb08666a7cc635.rmeta: src/lib.rs src/math/mod.rs
+
+/Users/nguyenductrang/Desktop/Contract/lib/lib_rust_dev/target/debug/deps/safemath_dev-3adb08666a7cc635.d: src/lib.rs src/math/mod.rs
+
+src/lib.rs:
+src/math/mod.rs:
